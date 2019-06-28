2f0fca927f1d0e55677b7b07069dbfbc0ac398dd
Lekerss <annas9001@gmail.com>
2019-06-28 17:41:11 +0200

    Slider functional. Closes #4

--- Controller/DecouvrirViewController.swift
+++ Controller/DecouvrirViewController.swift
@@ -9,28 +9,122 @@
 import UIKit
 
 class DecouvrirViewController: UIViewController {
+    
+    enum SliderState {
+        case expanded, collapsed
+    }
+    
+    var sliderViewController: SliderViewController!
+    var visualEffectView: UIVisualEffectView!
+    
+    let sliderHeight: CGFloat = 800
+    let sliderHandleAreaHeight: CGFloat = 180
+    
+    var sliderVisible = false
+    var nextState:SliderState {
+        return sliderVisible ? .collapsed : .expanded
+    }
+    
+    var runningAnimations = [UIViewPropertyAnimator]()
+    var animationProgressWhenInterrupted:CGFloat = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupSlider()
 
         // Do any additional setup after loading the view.
     }
     
 
-//    override func viewWillAppear(_ animated: Bool) {
-//        self.navigationController?.isNavigationBarHidden = true
-//    }
-//    override func viewDidDisappear(_ animated: Bool) {
-//        self.navigationController?.isNavigationBarHidden = false
-//    }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewWillAppear(_ animated: Bool) {
+        self.navigationController?.isNavigationBarHidden = true
     }
-    */
+    override func viewDidDisappear(_ animated: Bool) {
+        self.navigationController?.isNavigationBarHidden = false
+    }
+    
+    func setupSlider() {
+        visualEffectView = UIVisualEffectView()
+        visualEffectView.frame = self.view.frame
+        self.view.addSubview(visualEffectView)
+        
+        sliderViewController = SliderViewController(nibName: "SliderViewController", bundle:nil)
+        self.addChild(sliderViewController)
+        self.view.addSubview(sliderViewController.view)
+        
+        sliderViewController.view.frame = CGRect(x: 0, y: self.view.frame.height - sliderHandleAreaHeight, width: self.view.bounds.width, height: sliderHeight)
+        
+        sliderViewController.view.clipsToBounds = true
+        
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(DecouvrirViewController.handleSliderTap(recognizer:)))
 
+        let panGestureRecognizer = UIPanGestureRecognizer(target: self, action: #selector(DecouvrirViewController.handleSliderPan(recognizer:)))
+
+        sliderViewController.handleArea.addGestureRecognizer(tapGestureRecognizer)
+        sliderViewController.handleArea.addGestureRecognizer(panGestureRecognizer)
+
+    }
+    
+    @objc
+    func handleSliderTap(recognizer:UITapGestureRecognizer) {
+        
+    }
+    
+    
+    @objc
+    func handleSliderPan(recognizer:UIPanGestureRecognizer) {
+        switch recognizer.state {
+        case .began:
+            startInteractiveTransition(state: nextState, duration: 0.5)
+        case .changed:
+            updateInteractiveTransition(fractionCompleted: 0)
+        case .ended:
+            continueInteractiveTransition()
+        default:
+            break
+        }
+    }
+    
+    func animateTransitionIfNeeded(state:SliderState, duration: TimeInterval) {
+        if runningAnimations.isEmpty {
+            let frameAnimator = UIViewPropertyAnimator(duration: duration, dampingRatio: 1) {
+                switch state {
+                case .expanded:
+                    self.sliderViewController.view.frame.origin.y = self.view.frame.height - self.sliderHeight
+                case .collapsed:
+                    self.sliderViewController.view.frame.origin.y = self.view.frame.height - self.sliderHandleAreaHeight
+                }
+            }
+            
+            frameAnimator.addCompletion { _ in
+                self.sliderVisible = !self.sliderVisible
+                self.runningAnimations.removeAll()
+            }
+            
+            frameAnimator.startAnimation()
+            self.runningAnimations.append(frameAnimator)
+        }
+    }
+    
+    func startInteractiveTransition(state:SliderState, duration: TimeInterval) {
+        if runningAnimations.isEmpty {
+            animateTransitionIfNeeded(state: state, duration: duration)
+        }
+        for animator in runningAnimations {
+            animator.pauseAnimation()
+            animationProgressWhenInterrupted = animator.fractionComplete
+        }
+    }
+    
+    func updateInteractiveTransition(fractionCompleted: CGFloat) {
+        for animator in runningAnimations {
+            animator.fractionComplete = fractionCompleted + animationProgressWhenInterrupted
+        }
+    }
+    
+    func continueInteractiveTransition () {
+        for animator in runningAnimations {
+            animator.continueAnimation(withTimingParameters: nil, durationFactor: 0)
+        }
+    }
 }
---
+++ Controller/SliderViewController.swift
@@ -0,0 +1,32 @@
+//
+//  SliderViewController.swift
+//  TripHunters
+//
+//  Created by Annas Saker on 28/06/2019.
+//  Copyright © 2019 oscar Amzalag. All rights reserved.
+//
+
+import UIKit
+
+class SliderViewController: UIViewController {
+    @IBOutlet weak var handleArea: UIView!
+    @IBOutlet weak var handleLine: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        handleLine.layer.cornerRadius = 5
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
