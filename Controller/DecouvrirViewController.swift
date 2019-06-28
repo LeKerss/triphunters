4b33142f359fa8f96a684a195ee4fdc725c221c2
oscaramz <oscaramzalag@hotmail.com>
2019-06-28 10:42:59 +0200

    Initial Commit

---
+++ Controller/DecouvrirViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DecouvrirViewController.swift
+//  TripHunters
+//
+//  Created by oscar Amzalag on 28/06/2019.
+//  Copyright © 2019 oscar Amzalag. All rights reserved.
+//
+
+import UIKit
+
+class DecouvrirViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Controller/LoginViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TripHunters
+//
+//  Created by oscar Amzalag on 28/06/2019.
+//  Copyright © 2019 oscar Amzalag. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
