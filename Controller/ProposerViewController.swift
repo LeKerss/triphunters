23482d33d59a96a844fb13331cc1f5fe349012aa
Janin Culhaoglu <janin.culhaoglu@gmail.com>
2019-06-28 17:44:44 +0200

    Mise a jour écran d'ajout activité

--- Controller/ProposerViewController.swift
+++ Controller/ProposerViewController.swift
@@ -8,23 +8,28 @@
 
 import UIKit
 
-class ProposerViewController: UIViewController {
+class ProposerViewController: UITableViewController {
 
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var placeTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func cancelButton(_ sender: Any) {
+        
     }
-    */
+    
+    @IBAction func addImage(_ sender: Any) {
 
+    }
+
+    @IBAction func daySwitch(_ sender: UISwitch) {
+
+    }
+    
+    @IBAction func createButton(_ sender: Any) {
+
+    }
 }
