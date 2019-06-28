c14b16583fc9c494606b438843fc6dc869b69a6b
oscaramz <oscaramzalag@hotmail.com>
2019-06-28 11:30:47 +0200

    add struct activity favori & inscription

---
+++ Models/InscriptionActivity.swift
@@ -0,0 +1,16 @@
+//
+//  InscriptionActivity.swift
+//  TripHunters
+//
+//  Created by oscar Amzalag on 28/06/2019.
+//  Copyright © 2019 oscar Amzalag. All rights reserved.
+//
+
+import Foundation
+
+struct InscriptionActivity {
+    
+    let idActivity : Int
+    let idUser: Int
+    let dateInscription: Date
+}
