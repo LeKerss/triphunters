ca01292ba82eb54c7de6b4cf0e4c88526e5d7f9c
Chevalier Simon <sim.chevalier@gmail.com>
2019-07-07 13:33:58 +0200

    TableView "voir Plus" fait

--- Controller/myActivityTableViewController.swift
+++ Controller/myActivityTableViewController.swift
@@ -7,55 +7,53 @@
 //
 
 import UIKit
+import MapKit
+
 
 class myActivityTableViewController: UITableViewController {
     
-    var activitySelected:Activity? = nil
-    
-    var listActivty = [Activity(idActivity: 1, idUser: 1, nameActivity: "Wall Street Pigalle", descriptionActivity: "Un Concept Bar Unique à Paris. \n\nÀ partir de 18h le Concept Bar Wall Street Pigalle à Paris se transforme en place boursière pour vous proposer un large portefeuille boursier composé de verres de vins, de pintes de bières et de nombreux cocktails. \n\n5 écrans géants vous permettent de suivre le cours des boissons en direct dont la tendance changera toutes les 100 secondes ! \n\nUn verre peut ainsi passer de 12 à 4 euros en quelques secondes et même subir une décote sans précédent lors d’un des nombreux krachs boursiers qui viendront pimenter chaque soirée.", typeActivity: .NightLife, adresse: "49 Boulevard de Clichy, 75009 Paris", country: "FranceTest", gpsx: 48.88315, gpsy: 2.333989, showActivity: true, imageDesc: [UIImage(named: "activityWallStreet1")!, UIImage(named: "activityWallStreet2")!]), Activity(idActivity: 2, idUser: 2, nameActivity: "Le musée du Louvre", descriptionActivity: "Avec plus de 460.000 œuvres intemporelles de l’Antiquité à nos jours, le Musée du Louvre est l’un des plus grands musées d’art et d’histoire du monde. \n\nVous y découvrirez des œuvres mondialement connues, telles que La Joconde, La Vénus de Milo, le Radeau de la Méduse, La Liberté guidant le peuple, La mort de la Vierge, La Dentellière, Le Sacre de Napoléon, le Portrait de Louis XIV en costume de sacre, etc.", typeActivity: .Cultural, adresse: "99, rue de Rivoli, 75001 Paris", country: "FranceTest", gpsx: 48.864824, gpsy: 2.334595, showActivity: true, imageDesc: [UIImage(named: "louvre1")!, UIImage(named: "louvre2")!, UIImage(named: "louvre3")!, UIImage(named: "louvre4")!])]
-    
+    var listActivty : [Activity] = []
     var name : String = ""
+    
+    var activitySelected:Activity? = nil
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
-        
         navigationItem.title = name
         
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
 
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 1
+        return listActivty.count
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return listActivty.count
+        return 1
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-        let cell = tableView.dequeueReusableCell(withIdentifier: "activityCell", for: indexPath)
-        
-        cell.textLabel?.text = listActivty[indexPath.row].nameActivity
-        cell.detailTextLabel!.text = listActivty[indexPath.row].typeActivity.name()
-        cell.imageView?.image = listActivty[indexPath.row].imageDesc[0]
-        
-        return cell
+        if let cell : TableActivityViewCell = tableView.dequeueReusableCell(withIdentifier: "activityCell", for: indexPath) as? TableActivityViewCell{
+            cell.initListImages(myActivity: listActivty[indexPath.section])
+            cell.nameActivity.text = listActivty[indexPath.section].nameActivity
+            cell.imageCountry.image = UIImage(named: listActivty[indexPath.section].country)
+            cell.categoryLabel.text = listActivty[indexPath.section].typeActivity.name()
+            cell.categoryLabel.textColor = listActivty[indexPath.section].typeActivity.color
+            if let userLocation = lm.location {
+                let dist = userLocation.distance(from: CLLocation(latitude: CLLocationDegrees(floatLiteral: listActivty[indexPath.section].gpsx), longitude: CLLocationDegrees(floatLiteral: listActivty[indexPath.section].gpsy)))
+                cell.distanceLabel.text = String(Int(dist)) + "m"
+            }
+            cell.descLabel.text = listActivty[indexPath.section].descriptionActivity
+            return cell
+        }
+        return UITableViewCell()
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        activitySelected = listActivty[indexPath.row]
+        activitySelected = listActivty[indexPath.section]
         performSegue(withIdentifier: "showActivityFromList", sender: nil)
     }
     
@@ -65,60 +63,5 @@ class myActivityTableViewController: UITableViewController {
         destinationViewController?.currentActivity = activitySelected!
         
     }
-
-    
-//    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-//        if (editingStyle == .delete){
-//            let optionMenu = UIAlertController(title: nil, message: "Voulez vous vraiment supprimer votre commentaire ?", preferredStyle: .actionSheet)
-//            let deleteAction = UIAlertAction(title: "Oui", style: .default, handler: { action in
-//                if (editingStyle == .delete){
-//                    self.commentOnActivity.remove(at: indexPath.row)
-//                    self.tableView.reloadData()
-//                    self.navigationItem.title = "\(self.commentOnActivity.count) Commentaires"
-//                }
-//            })
-//            let cancelAction = UIAlertAction(title: "Annuler", style: .cancel)
-//            optionMenu.addAction(deleteAction)
-//            optionMenu.addAction(cancelAction)
-//            self.present(optionMenu, animated: true )
-//        }
-//    }
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
